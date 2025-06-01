53a6a235f2bdbc84b1dbd6451076433b81e540c2
JuniorBandeira7 <juliobandeira77@gmail.com>
2025-06-01 19:30:44 -0300

    up

---
+++ templates/Categories/consultar.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Category $category
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Alterar Category'), ['action' => 'alterar', $category->id], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Excluir Category'), ['action' => 'excluir', $category->id], ['confirm' => __('Are you sure you want to delete # {0}?', $category->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Categories'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('New Category'), ['action' => 'incluir'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="categories view content">
+            <h3><?= h($category->name) ?></h3>
+            <table>
+                <tr>
+                    <th><?= __('Name') ?></th>
+                    <td><?= h($category->name) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <td><?= $this->Number->format($category->id) ?></td>
+                </tr>
+            </table>
+            <div class="text">
+                <strong><?= __('Description') ?></strong>
+                <blockquote>
+                    <?= $this->Text->autoParagraph(h($category->description)); ?>
+                </blockquote>
+            </div>
+            <div class="related">
+                <h4><?= __('Related Products') ?></h4>
+                <?php if (!empty($category->products)) : ?>
+                <div class="table-responsive">
+                    <table>
+                        <tr>
+                            <th><?= __('Id') ?></th>
+                            <th><?= __('Name') ?></th>
+                            <th><?= __('Description') ?></th>
+                            <th><?= __('Price') ?></th>
+                            <th><?= __('Category Id') ?></th>
+                            <th class="actions"><?= __('Actions') ?></th>
+                        </tr>
+                        <?php foreach ($category->products as $products) : ?>
+                        <tr>
+                            <td><?= h($products->id) ?></td>
+                            <td><?= h($products->name) ?></td>
+                            <td><?= h($products->description) ?></td>
+                            <td><?= h($products->price) ?></td>
+                            <td><?= h($products->category_id) ?></td>
+                            <td class="actions">
+                                <?= $this->Html->link(__('Consultar'), ['controller' => 'Products', 'action' => 'consultar', $products->id]) ?>
+                                <?= $this->Html->link(__('Alterar'), ['controller' => 'Products', 'action' => 'alterar', $products->id]) ?>
+                                <?= $this->Form->postLink(__('Excluir'), ['controller' => 'Products', 'action' => 'excluir', $products->id], ['confirm' => __('Are you sure you want to delete # {0}?', $products->id)]) ?>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
